f0e56d0e23315845e6fd67c08b6af866e21793c7
LUCIFER49 <p1ush.prj@gmail.com>
2024-01-01 23:13:28 +0530

    Setting up Backend

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -16,7 +16,7 @@ const ProductScreen = () => {
       </Link>
       <Row>
         <Col md={5}>
-          <Image src={product.image} alt={product.name} fluid />
+          <img src={product.image} alt={product.name} fluid />
         </Col>
         <Col md={4}>
           <ListGroup variant="flush">
@@ -30,6 +30,8 @@ const ProductScreen = () => {
               />
             </ListGroup.Item>
             <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+            {/* Product Description */}
+            <ListGroup.Item>Description: {product.description}</ListGroup.Item>
           </ListGroup>
         </Col>
         <Col md={3}>
@@ -52,6 +54,10 @@ const ProductScreen = () => {
                         <strong>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</strong>
                     </Col>
                 </Row>
+              </ListGroup.Item>
+              {/* For Add to Cart button */}
+              <ListGroup.Item>
+                <Button className="btn-block" type="button" disabled={product.countInStock === 0} > Add To Cart</Button>
               </ListGroup.Item>
             </ListGroup>
           </Card>
