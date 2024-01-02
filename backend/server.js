40fb379171034231dbbc1f861efd255381a58f52
LUCIFER49 <p1ush.prj@gmail.com>
2024-01-02 22:27:14 +0530

    Setup Completed for Backend

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,26 @@
 import express from "express";
-// import products from "./data/products";
-const port = 5000;
+import dotenv from 'dotenv';
+// Function should be declare before any of the env file declared variables
+dotenv.config();      
+import products from './data/products.js';
+const port = process.env.PORT || 5000;
 
 const app = express();
 
+// Route for home page
 app.get("/", (req, res) => {
   res.send("API is running");
 });
+
+// Route for all the products
+app.get('/api/products', (req, res) => {
+    res.json(products);
+});
+
+// Route for single products
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+})
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
