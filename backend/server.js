aaeb00f4969454d30dd0bb9426436024c88df397
LUCIFER49 <p1ush.prj@gmail.com>
2024-01-21 23:08:01 +0530

    Model Creation for MongoDB

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import connectDB from './config/db.js';
 import products from './data/products.js';
 const port = process.env.PORT || 5000;
 
-connectDB();
+connectDB();    // Connect to MongoDB
 
 const app = express();
 
