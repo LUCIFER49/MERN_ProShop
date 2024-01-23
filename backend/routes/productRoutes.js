9edf7d7b7fe7e187cfa8ffb32059576b2495f7f9
LUCIFER49 <p1ush.prj@gmail.com>
2024-01-23 22:35:42 +0530

    Adding Async Handler

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,16 +1,25 @@
 import express from "express";
 const router = express.Router();
-import products from '../data/products.js';
+import asyncHandler from "../middleware/asyncHandler.js";
+import Product from "../models/productModel.js";
 
 // Route for all the products
-router.get('/', async (req, res) => {
+router.get("/", asyncHandler(async (req, res) => {
+    const products = await Product.find({}); //'{}': empty object for all products or pass some options for limited products
     res.json(products);
-});
+  })
+);
 
 // Route for single products
-router.get('/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-})
+router.get("/:id", asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      return res.json(product);
+    }
+
+    res.status(404).json({ message: "Product not found" });
+  })
+);
 
 export default router;
