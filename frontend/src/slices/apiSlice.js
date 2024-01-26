d611261a53fe21924c9c64e45496bfb456ed919d
LUCIFER49 <p1ush.prj@gmail.com>
2024-01-26 19:28:56 +0530

    Redux Slices Setup

---
+++ frontend/src/slices/apiSlice.js
@@ -0,0 +1,11 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { BASE_URL } from "../constants";
+
+const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
+
+export const apiSlice = createApi({
+  baseQuery,
+  tagTypes: ["Product", "Order", "User"],
+  // we don't have to manually fetch data(try catch api) because of the 'builder'
+  endpoints: (builder) => ({}),
+});
