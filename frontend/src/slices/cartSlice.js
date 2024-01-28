a1304148c82d4bae53bc89f3f95bd1c2a82116f9
LUCIFER49 <p1ush.prj@gmail.com>
2024-01-28 12:26:13 +0530

    Adding CartSlice

---
+++ frontend/src/slices/cartSlice.js
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : { cartItems: [] }; //localStorage can only holds string in object format
+
+const addDecimals = (num) => {
+    return (Math.round(num * 100) / 100).toFixed(2);
+}
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {      //state: current state of cart && action: any data in payload
+      const item = action.payload;
+
+      const existItem = state.cartItems.find((x) => x._id === item._id);
+
+      if (existItem) {
+        state.cartItems = state.cartItems.map((x) => x._id === existItem._id ? item : x);
+      } else {
+        state.cartItems = [...state.cartItems, item];
+      }
+
+      //CalculateItem Price
+      state.itemsPrice = addDecimals(state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0));
+
+      //Calculate Shipping Price (If order is over )
+      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
+
+      //Calculate Tax Price (18% Tax)
+    //   state.taxPrice
+
+      //Calculate Total Price
+    },
+  }, //it will contains all the functions(or actions) that are related to cart
+});
+
+export default cartSlice.reducer;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,12 @@
 // boiler plate setup
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./slices/apiSlice";
+import cartSliceReducer from "./slices/cartSlice";
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
+    cart: cartSliceReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: true,  // only in development
