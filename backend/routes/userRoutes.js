50c2df392d76135ce53453730b93309e9dfbe6a0
LUCIFER49 <p1ush.prj@gmail.com>
2024-02-14 22:16:34 +0530

    Added User Routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 const router = express.Router();
-import { authUser, registerUser, logoutUser, getUserProfile, updateUserProfile, getUsers, deleteUser, getUserByID, updateUser } from "../controllers/userController"
+import { authUser, registerUser, logoutUser, getUserProfile, updateUserProfile, getUsers, deleteUser, getUserByID, updateUser } from "../controllers/userController.js"
 
 router.route('/').post(registerUser).get(getUsers);
 
@@ -10,7 +10,7 @@ router.post('/login', authUser);
 
 router.route('/profile').get(getUserProfile).put(updateUserProfile);
 
-router.route('/:id');
+router.route('/:id').delete(deleteUser).get(getUserByID).put(updateUser);
 
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();        // Function should be declare before any of the env file declared variables
 import connectDB from './config/db.js';
 import productRoutes from "./routes/productRoutes.js";
-import { notFound, errorHandler } from "./middleware/errorMindleware.js";
+import userRoutes from "./routes/userRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 const port = process.env.PORT || 5000;
 
 connectDB();    // Connect to MongoDB
@@ -16,6 +17,7 @@ app.get("/", (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
