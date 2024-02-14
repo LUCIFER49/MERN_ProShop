6b605be9fbcf8b286885d96ca6e207744b579eb3
LUCIFER49 <p1ush.prj@gmail.com>
2024-02-15 00:42:50 +0530

    Setting up responses for User Routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,21 @@ import User from "../models/userModel.js";
 // @route    POST /api/users/login
 // @access   Public
 const authUser = asyncHandler(async (req, res) => {
-  res.send("Auth user");
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
 });
 
 // @desc     Register user
@@ -43,12 +57,11 @@ const getUsers = asyncHandler(async (req, res) => {
   res.send("get users");
 });
 
-
 // @desc     Get User by ID
 // @route    GET /api/users/:id
 // @access   Private/Admin
 const getUserByID = asyncHandler(async (req, res) => {
-    res.send("get user by id");
+  res.send("get user by id");
 });
 
 // @desc     Delete user
@@ -62,7 +75,17 @@ const deleteUser = asyncHandler(async (req, res) => {
 // @route    PUT /api/users/:id
 // @access   Private/Admin
 const updateUser = asyncHandler(async (req, res) => {
-  res.send('update user');
+  res.send("update user");
 });
 
-export { authUser, registerUser, logoutUser, getUserProfile, updateUserProfile, getUsers, deleteUser, getUserByID, updateUser }
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserByID,
+  updateUser,
+};
--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,10 @@ connectDB();    // Connect to MongoDB
 
 const app = express();
 
+// Body Parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Route for home page
 app.get("/", (req, res) => {
   res.send("API is running");
