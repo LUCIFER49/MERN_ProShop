63ef9e5d0785d87ba7c309298e18266c2651678a
LUCIFER49 <p1ush.prj@gmail.com>
2024-02-17 02:08:26 +0530

    Added JWT package

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/userModel.js";
+import jwt from "jsonwebtoken";
 
 // @desc     Auth user & get token
 // @route    POST /api/users/login
@@ -18,7 +19,7 @@ const authUser = asyncHandler(async (req, res) => {
     });
   } else {
     res.status(401);
-    throw new Error('Invalid email or password');
+    throw new Error("Invalid email or password");
   }
 });
 
