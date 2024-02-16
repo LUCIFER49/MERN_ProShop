414bc242eb2589abbe378648d797170393fd780b
LUCIFER49 <p1ush.prj@gmail.com>
2024-02-17 04:21:35 +0530

    Integrating JWT in Different Routes

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
 import express from "express";
-import dotenv from 'dotenv';
-dotenv.config();        // Function should be declare before any of the env file declared variables
-import connectDB from './config/db.js';
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+dotenv.config(); // Function should be declare before any of the env file declared variables
+import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 const port = process.env.PORT || 5000;
 
-connectDB();    // Connect to MongoDB
+connectDB(); // Connect to MongoDB
 
 const app = express();
 
@@ -15,16 +16,18 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Cookie parser middleware
+app.use(cookieParser());
+
 // Route for home page
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.use('/api/products', productRoutes);
-app.use('/api/users', userRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
-
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
