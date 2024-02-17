4f994925c182acce47dce5609d9f45de381fd77d
LUCIFER49 <p1ush.prj@gmail.com>
2024-02-17 17:51:49 +0530

    Integrating JWT in Different Routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,16 +11,15 @@ const authUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
-
-    const token = jwt.sign({ userId:user._id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "30d" });
 
     // Set JWT as HTTP-Only cookie
-    res.cookie('jwt', token, {
+    res.cookie("jwt", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV !== 'development',
-      sameSite: 'strict',
-      maxAge: 30 * 24 * 60 * 60 * 1000   //30 days
-    })
+      secure: process.env.NODE_ENV !== "development",
+      sameSite: "strict",
+      maxAge: 30 * 24 * 60 * 60 * 1000, //30 days
+    });
 
     res.json({
       _id: user._id,
@@ -38,14 +37,40 @@ const authUser = asyncHandler(async (req, res) => {
 // @route    POST /api/users
 // @access   Public
 const registerUser = asyncHandler(async (req, res) => {
-  res.send("register user");
+  const { name, email, password} = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({ name, email, password });
+  
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid User data");
+  }
 });
 
 // @desc     Logout user / clear cookie
 // @route    POST /api/users/logout
 // @access   Private
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send("logout user");
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    expires: new Date(0)
+  });
+
+  res.status(200).json({ message: 'Logged out successfully' });
 });
 
 // @desc     Get User Profile
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -13,9 +13,10 @@ const protect = asyncHandler(async (req, res, next) => {
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = await User.findById(decoded.userId).select("-password");
+      next();
     } catch (error) {
-      res.status(401);
       console.log(error);
+      res.status(401);
       throw new Error("Not Authorized, token failed");
     }
   } else {
