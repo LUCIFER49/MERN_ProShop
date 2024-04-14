af1ce9b39b13bc6ebda6a60a04d17fcb997ca374
LUCIFER49 <p1ush.prj@gmail.com>
2024-04-14 17:51:29 +0530

    Adding Payment Screen & making it protected route

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider,} from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
 import "./assets/styles/index.css";
@@ -14,22 +14,24 @@ import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ShippingScreen from "./screens/ShippingScreen";
+import PaymentScreen from "./screens/PaymentScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<App/>}>
-      <Route index={true} path='/' element={<HomeScreen/>} />
-      <Route path='/product/:id' element={<ProductScreen/>} />
-      <Route path='/cart' element={<CartScreen/>} />
-      <Route path='/login' element={<LoginScreen/>} />
-      <Route path='/register' element={<RegisterScreen/>} />
-      
-      <Route path="" element={<PrivateRoute/>} >
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
+      <Route path="/cart" element={<CartScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
+      <Route path="/register" element={<RegisterScreen />} />
+
+      <Route path="" element={<PrivateRoute />}>
         <Route path="/shipping" element={<ShippingScreen />} />
+        <Route path="/payment" element={<PaymentScreen />} />
       </Route>
     </Route>
   )
-)
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
