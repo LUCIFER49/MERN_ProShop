3d5ba5a42d67fe59b7f8667357c34a2dcdc19325
LUCIFER49 <p1ush.prj@gmail.com>
2024-06-03 00:18:06 +0530

    Adding Build Details

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
-import uploadRoutes from './routes/uploadRoutes.js'
+import uploadRoutes from "./routes/uploadRoutes.js";
 const port = process.env.PORT || 5000;
 
 connectDB(); // Connect to MongoDB
@@ -22,22 +22,33 @@ app.use(express.urlencoded({ extended: true }));
 // Cookie parser middleware
 app.use(cookieParser());
 
-// Route for home page
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
-
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/upload", uploadRoutes);
 
+app.get("/api/config/paypal", (req, res) =>
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
+);
 
-app.get("/api/config/paypal", (req, res) => res.send({ clientId: process.env.PAYPAL_CLIENT_ID}));
+// Make uploads folder a static
+const __dirname = path.resolve(); // Set __dirname to current directory
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-// Make uploads folder a static 
-const __dirname = path.resolve();  // Set __dirname to current directory
-app.use('/uploads', express.static(path.join(__dirname, '/uploads'))); 
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  // any route that is not api will be redirected to index.html
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  // Route for home page
+  app.get("/", (req, res) => {
+    res.send("API is running..... ");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
